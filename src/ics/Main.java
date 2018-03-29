1165079d5079f65afadb4a0e7eb52d81af7a4bc9
Campiotti <adriano.campiotti@gmail.com>
2018-03-29 22:19:26 +0200

    added a few restrictions and limitations to the window in main also minor updates (unfinished) in answerBroker.

--- src/ics/Main.java
+++ src/ics/Main.java
@@ -1,5 +1,7 @@
 package ics;
 
+import controller.IController;
+import helper.Factory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,12 +9,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    IController mainController = new Factory().getController("MainController");
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 1280, 1024);
+        primaryStage.setMinWidth(1280);
+        primaryStage.setMinHeight(1024);
+        primaryStage.setMaxWidth(1920);
+        primaryStage.setMaxHeight(1080);
+        primaryStage.setResizable(true);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
--- src/persistence/AnswerBroker.java
+++ src/persistence/AnswerBroker.java
@@ -6,6 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AnswerBroker extends Broker<Answer>{
+    private static AnswerBroker instance;
+
+    public AnswerBroker(){}
+
+
     @Override
     protected Answer makeObject(ResultSet rs) throws SQLException {
         return null;
