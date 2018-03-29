5567a36f297453fe05cba7b926e650ec44e97692
Campiotti <adriano.campiotti@gmail.com>
2018-03-29 23:01:27 +0200

    added a todo

--- src/persistence/AnswerBroker.java
+++ src/persistence/AnswerBroker.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class AnswerBroker extends Broker<Answer>{
     private static AnswerBroker instance;
-
+    //TODO basically rip addressbroker from grüner into this thing. (also need create table if not exists statements somewhere)
     public AnswerBroker(){}
 
 
